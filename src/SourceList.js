0bcc646ad7f9a681f0a39fb4441c878dc578ef33
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-07-14 21:09:09 +0530

    corrected for loop

--- src/SourceList.js
+++ src/SourceList.js
@@ -1,35 +1,32 @@
-import React from 'react'
-import * as ReactBootStrap from 'react-bootstrap'
-export default class List extends React.Component
-{
-    // key=2c27260665d5434cb5c68ead5b2e3cb9
-    state={
-        loading:true,
-        data:[]
-    }
+import React from 'react';
+import * as ReactBootStrap from 'react-bootstrap';
+export default class List extends React.Component {
+	// key=2c27260665d5434cb5c68ead5b2e3cb9
+	state = {
+		loading: true,
+		data: [],
+	};
 
-    async componentDidMount(){
-        const url="https://newsapi.org/v2/sources?apiKey=2c27260665d5434cb5c68ead5b2e3cb9"
-        const response= await fetch(url);
-        const channels =await response.json();
-        console.log(channels)
-        this.setState({
-            data:channels
-        })
-        
-    }
-    newsList(){
-        return(
-            <tr>
-                <td>{this.state.data[0]}</td>
-            </tr>
-        )
-        
-    }
-    render() {return(
-        <ul>
-        {this.state.data.map(this.newsList)}
-    </ul>
-        
-    )}
+	async componentDidMount() {
+		const url =
+			'https://newsapi.org/v2/sources?apiKey=2c27260665d5434cb5c68ead5b2e3cb9';
+		const response = await fetch(url);
+		const channels = await response.json();
+		this.setState(
+			{
+				data: channels.sources,
+			},
+			() => {}
+		);
+	}
+	render() {
+		const channelList = this.state.data.map((channel) => {
+			return (
+				<div>
+					<p key={channel.id}>{channel.id}</p>
+				</div>
+			);
+		});
+		return <div>{channelList}</div>;
+	}
 }
